1e94cac96e8da682422f94ad918be9989fca89b9
sbojarovski <bojarovski@gmail.com>
2015-12-24 23:20:29 +0100

    changed default constructor for face
    The 9 cells are now explicitly named, but still contained in a vector.
    I have also added  some state querying methods

--- CubeFace.cpp
+++ CubeFace.cpp
@@ -1,12 +1,55 @@
 //
 // Created by stefan on 12/24/15.
 //
-
+#include <algorithm>
 #include "Cube.h"
 #include "CubeFace.h"
 #include "Cell.h"
 
-void CubeFace::fill(const CellColor &color) {
-    for (Cell c : cells)
+CubeFace::CubeFace(const CellColor & color)
+        :c11(1, 1, color),
+         c12(1, 2, color),
+         c13(1, 3, color),
+         c21(2, 1, color),
+         c22(2, 2, color),
+         c23(2, 3, color),
+         c31(3, 1, color),
+         c32(3, 2, color),
+         c33(3, 3, color)
+{
+    cells.push_back(c11);
+    cells.push_back(c12);
+    cells.push_back(c13);
+    cells.push_back(c21);
+    cells.push_back(c22);
+    cells.push_back(c23);
+    cells.push_back(c31);
+    cells.push_back(c32);
+    cells.push_back(c33);
+    centerColor = color;
+}
+
+void CubeFace::fill(const CellColor & color) {
+    for (auto & c : cells)
         c.setColor(color);
+}
+
+bool CubeFace::isSolved() const {
+    std::vector<bool> isCenterColor;
+    // map: are all cells in the face the same (center) color
+    std::transform(cells.begin(), cells.end(),
+                   std::back_inserter(isCenterColor),
+                   [](const Cell & c){return c.getColor() == centerColor;});
+    bool solved = false;
+    // reduce
+    // TODO: solved was not captured, what was that? ([&solved](...){...})
+    // TODO: vector<bool> cannot return reference, check into it
+    solved = std::accumulate(cells.begin(), cells.end(),
+                    true,
+                    [](bool current, bool next){return current && next;});
+    return solved;
+}
+
+bool CubeFace::isCenterCorrect() const {
+    return c22.getColor() == centerColor;
 }
--- CubeFace.h
+++ CubeFace.h
@@ -5,16 +5,32 @@
 #ifndef RUBIC_SOLVER_CUBEFACE_H
 #define RUBIC_SOLVER_CUBEFACE_H
 
+#include <vector>
 #include "Cell.h"
 
 class CubeFace
 {
 public:
+    void CubeFace(const CellColor & color);
     void fill(const CellColor & color);
-private:
-    std::vector<Cell> cells;
-};
+    bool isSolved() const;
+    bool isCenterCorrect() const;
+    const std::vector<Cell &> &getCells() const {
+        return cells;
+    }
 
-#include <vector>
+private:
+    std::vector<Cell &> cells;
+    CellColor centerColor;
+    Cell c11;
+    Cell c12;
+    Cell c13;
+    Cell c21;
+    Cell c22;
+    Cell c23;
+    Cell c31;
+    Cell c32;
+    Cell c33;
+};
 
 #endif //RUBIC_SOLVER_CUBEFACE_H
