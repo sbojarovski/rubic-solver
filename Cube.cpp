00b00780ef2ede38658cc1443f6ffa4c5dafc3bc
sbojarovski <bojarovski@gmail.com>
2015-12-24 18:08:38 +0100

    add transforms and state descripting members

--- Cube.cpp
+++ Cube.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Cube.h"
+#include "CubeFace.h"
+#include "Cell.h"
+
+Cube::Cube() :
+        solved(true),
+        valid(true)
+{
+    front.fill(CellColor::WHITE);
+    back.fill(CellColor::YELLOW);
+    left.fill(CellColor::GREEN);
+    right.fill(CellColor::BLUE);
+    top.fill(CellColor::ORANGE);
+    bottom.fill(CellColor::RED);
+}
+
+void Cube::transform(const CubeTransforms &transform) {
+
+}
+
+bool Cube::isValid() const {
+    return false;
+}
+
+void Cube::scramble() {
+
+}
+
+void Cube::solve() {
+
+}
--- Cube.h
+++ Cube.h
@@ -5,20 +5,12 @@
 #ifndef RUBIC_SOLVER_CUBE_H
 #define RUBIC_SOLVER_CUBE_H
 
-enum CellColor
-{
-    UNKNOWN = -1,
-    WHITE,
-    YELLOW,
-    BLUE,
-    GREEN,
-    ORANGE,
-    RED
-};
+#include <vector>
+#include "CubeFace.h"
 
 enum CubeFacePosition
 {
-    FRONT,
+    FRONT = 1,
     BACK,
     LEFT,
     RIGHT,
@@ -26,25 +18,20 @@ enum CubeFacePosition
     BOTTOM
 };
 
-class CellPosition
+enum CubeTransforms
 {
-private:
-    CubeFacePosition face;
-    int row;
-    int column;
-};
-
-//
-// A cell is one face of a piece.
-// In the world, cells are of fixed color and they belong to one unique piece.
-// Then the pieces move around, and the cells and colors with them.
-// Here, I consider a cell to have a fixed position and a floating color.
-//
-class Cell
-{
-private:
-    CellColor color;
-    CellPosition position;
+    FRONT_CW = 1,
+    FRONT_CCW,
+    BACK_CW,
+    BACK_CCW,
+    LEFT_CW,
+    LEFT_CCW,
+    RIGHT_CW,
+    RIGHT_CCW,
+    TOP_CW,
+    TOP_CCW,
+    BOTTOM_CW,
+    BOTTOM_CCW
 };
 
 //
@@ -61,8 +48,14 @@ private:
     CubeFace right;
     CubeFace top;
     CubeFace bottom;
+    bool solved;
+    bool valid;
 public:
-    bool isSolved();
+    void Cube();
+    void transform(const CubeTransforms & transform);
+    // 9 cells of each color, center pieces all different etc.
+    bool isValid() const;
+    bool isSolved() const { return  solved;};
     void scramble();
     void solve();
 };
