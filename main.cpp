9f4baaf49d34b79c104637472a359c4beea45fe3
sbojarovski <bojarovski@gmail.com>
2016-09-23 21:58:08 +0200

    wrote a simple loop to debug cube transformations

--- main.cpp
+++ main.cpp
@@ -9,20 +9,32 @@
 #include "Cube.h"
 #include "CubeDrawer2D.h"
 
-int main(int, char**)
+void drawStepTransform(Cube & cube, std::vector<CubeTransforms > & transforms)
 {
     cv::Mat img(320, 320, CV_8UC3, cv::Scalar::all(0));
-
     cv::namedWindow("Rubik Solver", 1);
-    Cube cube;
-    cube.transform(LEFT_CCW);
-    cube.transform(TOP_CW);
-    cube.transform(BACK_CCW);
-    //cube.scramble(3);
-    CubeDrawer2D dCube(cube);
-    dCube.draw(&img, dCube.mDefaultColorMap);
-    imshow("Rubik Solver", img);
 
-    cv::waitKey();
+    for(auto t : transforms)
+    {
+        CubeDrawer2D dCube(cube);
+        dCube.draw(img, dCube.mDefaultColorMap);
+        imshow("Rubik Solver", img);
+        cv::waitKey();
+        cube.transform(t);
+    }
+}
+
+int main(int, char**)
+{
+    Cube cube;
+
+    std::vector<CubeTransforms > tvec = {LEFT_CCW,
+                                         TOP_CW,
+                                         BACK_CCW};
+
+    drawStepTransform(cube, tvec);
+
+    //cube.scramble(3);
+
     return 0;
 }
