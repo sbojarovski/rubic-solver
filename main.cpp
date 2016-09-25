6e13f4ec94aa93858e684fd1e7fa5561db165f21
sbojarovski <bojarovski@gmail.com>
2016-09-25 16:01:17 +0200

    wip on main

--- main.cpp
+++ main.cpp
@@ -11,17 +11,35 @@
 
 void drawStepTransform(Cube & cube, const std::vector<CubeTransforms > & transforms)
 {
-    cv::Mat img(320, 320, CV_8UC3, cv::Scalar::all(0));
-    cv::namedWindow("Rubik Solver", 1);
+    int imgWidth = 960;
+    int imgHeight = 960;
+    cv::Point labelPos(imgWidth - imgWidth/3, imgHeight - imgHeight/3);
+    cv::Point validPos(labelPos.x, labelPos.y + 30);
+    cv::Mat img(imgWidth, imgHeight, CV_8UC3, cv::Scalar::all(0));
+    cv::namedWindow("Rubik Solver", cv::WINDOW_FREERATIO | cv::WINDOW_NORMAL );
+
+    CubeDrawer2D dCube(cube);
 
     for(auto t : transforms)
     {
-        CubeDrawer2D dCube(cube);
+        // draw the cube and additional info
         dCube.draw(img);
+
+        std::string tLabel = Cube::getTransformString(t);
+        cv::rectangle(img, cv::Point(labelPos.x , labelPos.y + 10), cv::Point(labelPos.x + 220, labelPos.y - 20), CV_RGB(0, 0, 0), CV_FILLED);
+        cv::putText(img, tLabel, labelPos, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(200, 200, 200), 1.0, cv::LINE_AA);
+
+        cv::rectangle(img, cv::Point(validPos.x , validPos.y + 10), cv::Point(validPos.x + 220, validPos.y - 20), CV_RGB(0, 0, 0), CV_FILLED);
+        cv::putText(img, cube.isValid() ? "valid" : "not valid", validPos, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.0, CV_RGB(200, 200, 200), 1.0, cv::LINE_AA);
+
         imshow("Rubik Solver", img);
         cv::waitKey();
+        // apply the transformation
         cube.transform(t);
     }
+    dCube.draw(img);
+    imshow("Rubik Solver", img);
+    cv::waitKey();
 }
 
 void testAllTransforms()
@@ -61,6 +79,10 @@ void testAllTransforms()
 int main(int, char**)
 {
     Cube cube;
-    drawStepTransform(cube, Cube::getRandomTransformVec(10));
+    //testAllTransforms();
+    //drawStepTransform(cube, Cube::getRandomTransformVec(10));
+    // TODO: this causes an invalid state of the cube ...
+    std::vector<CubeTransforms > tvec = {TOP_CCW, LEFT_CCW};
+    drawStepTransform(cube, tvec);
     return 0;
 }
